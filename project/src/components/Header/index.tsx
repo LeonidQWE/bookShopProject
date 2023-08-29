abbda65ab6b56b742d8c049367d02205149ae063
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2023-08-29 14:09:30 +0300

    fix navbar

--- project/src/components/Header/index.tsx
+++ project/src/components/Header/index.tsx
@@ -5,16 +5,26 @@ import basket from '../../images/basket.svg'
 import user from '../../images/user.svg'
 
 export function Header(): JSX.Element {
+  function getLinkClass({ isActive }: { isActive: boolean }): string {
+    return isActive ? 'nav-link active' : 'nav-link'
+  }
+
   return (
     <header className="header">
       <NavLink to="/">
         <span className="header__logo">bookstore</span>
       </NavLink>
       <input className="header__search" placeholder="Search" type="text" />
-      <div>
-        <NavLink to="/favorite_books"><img src={favorite} alt=""></img></NavLink>
+      <div className="header__navbar">
+        <NavLink className={getLinkClass} to="/favorite_books">
+          <img src={favorite} alt=""></img>
+          <div className="header__active-marker"></div>
+        </NavLink>
         <img src={basket} alt=""></img>
-        <NavLink to="/user_page"><img src={user} alt=""></img></NavLink>
+        <NavLink className={getLinkClass} to="/user_page">
+          <img src={user} alt=""></img>
+          <div className="header__active-marker"></div>
+        </NavLink>
       </div>
     </header>
   )
